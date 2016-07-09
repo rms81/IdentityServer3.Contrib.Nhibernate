ed31896de903851c80baca0d1d3872963783d892
rms81 <rms81g@gmail.com>
2016-07-09 20:58:58 +0100

    Fix path in build.ps1

--- build.ps1
+++ build.ps1
@@ -3,12 +3,8 @@ Param(
 	[string]$preRelease = "beta"
 )
 
-gci .\source -Recurse "packages.config" |% {
-	"Restoring " + $_.FullName
-	.\source\.nuget\nuget.exe i $_.FullName -o .\source\packages
-}
 
-Import-Module .\source\packages\psake.4.4.6\tools\psake.psm1
+Import-Module .\src\packages\psake.4.6.0\tools\psake.psm1
 
 if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 	$buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
