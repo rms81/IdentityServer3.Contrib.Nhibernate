e0c45651b491e89e448dd5148aba4b014d14b057
rms81 <rms81g@gmail.com>
2016-07-09 21:14:16 +0100

    Nuget package restore in build.ps1

--- build.ps1
+++ build.ps1
@@ -3,12 +3,17 @@ Param(
 	[string]$preRelease = "beta"
 )
 
+gci .\src -Recurse "packages.config" |% {
+	"Restoring " + $_.FullName
+	.\src\.nuget\nuget.exe install $_.FullName -o .\src\packages
+}
 
 Import-Module .\src\packages\psake.4.6.0\tools\psake.psm1
 
 if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 	$buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
-	Write-Host "Using APPVEYOR_BUILD_NUMBER"
+	Write-Host "Using APPVEYOR_B
+	UILD_NUMBER"
 
 	$task = "appVeyor"
 }
